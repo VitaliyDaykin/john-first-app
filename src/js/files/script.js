4e7b060bdccce4cafe3f6195632799a1a5a6fc83
Diakin_Vitalii <vdakyn@gmail.com>
2022-11-05 01:50:06 +0200

    added scrollEffect

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,15 +4,54 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
-function updateLinks() {
-	const links = document.querySelectorAll(".menu__link");
-	const currentUrl = window.location;
-	for (const anchor of links) {
-		anchor.classList.remove("active");
-		const linkUrl = new URL(anchor.href);
-		if (linkUrl.pathname === currentUrl.pathname) {
-			anchor.classList.add("active");
+// function updateLinks() {
+// 	const links = document.querySelectorAll(".menu__link");
+// 	const currentUrl = window.location;
+// 	for (const anchor of links) {
+// 		anchor.classList.remove("active");
+// 		const linkUrl = new URL(anchor.href);
+// 		if (linkUrl.pathname === currentUrl.pathname) {
+// 			anchor.classList.add("active");
+// 		}
+// 	}
+// }
+// updateLinks();
+
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active');
+				}
+			}
 		}
 	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+			scrollTop = window.scrollY || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
 }
-updateLinks();
